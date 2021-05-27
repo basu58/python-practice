eed59d98cddbb36de7eb132de0397b6e4c89dd1b
Basudevc <basudevchhotray@gmail.com>
2021-05-27 11:58:06 +0530

    new

--- input1.py
+++ input1.py
@@ -1,3 +1,8 @@
-inp = "What is your age?\n"
-x = input(inp)
-print(x)
+inp1 = "Enter hours:\n"
+inp2 = "Enter rate:\n"
+x = float(input(inp1))
+y = float(input(inp2))
+#y = int(x)
+#print(int(x))
+print("pay: "+str(x * y))
+#print(type(y))
