5742421e2c4b88d0517382f478f780780fdb52a8
basu58 <basudevchhotray@gmail.com>
2021-06-23 19:15:41 +0530

    practice

--- x.py
+++ x.py
@@ -1,4 +1,17 @@
-import math, random
-for i in range(10):
-    x = random.random()
-    print(x)
+smallest = None
+print("Before:", smallest)
+for itervar in [3, 41, 12, 9, 74, 15]:
+    if smallest is None or itervar < smallest:
+        smallest = itervar
+        break
+    print("Loop:", itervar, smallest)
+print("Smallest:", smallest)
+for n in "banana":
+    print(n)
+str = "Hello Python"
+print(str[6:len(str)])
+print(len(str))
+word = "bananana"
+i = word.find("na")
+print(i)
+print(dir(word))
